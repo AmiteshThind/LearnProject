1ff95e704d3afbcc2b9fe0587f882e1f5c0a51bb
Amitesh.Thind@coxautoinc.com <Amitesh.Thind@coxautoinc.com>
2022-03-02 18:14:09 -0500

    added dashbaord where user can see all enrolled courses

--- learn-project-dapp/pages/user/dashboard.js
+++ learn-project-dapp/pages/user/dashboard.js
@@ -1,12 +1,103 @@
-import UserNavbar from "../../components/user/UserNavBar"
+import { useEffect, useState } from "react";
+import { useMoralis } from "react-moralis";
+import { Moralis } from "moralis";
+import UserNavbar from "../../components/user/UserNavBar";
+import Router, { useRouter } from "next/router";
+import { PlusCircleIcon } from "@heroicons/react/outline";
 
- 
 function Dashboard() {
-    return (
-        <div className=" bg-gradient-to-b  from-emerald-50  via-indigo-50 to-rose-100   h-screen">
-           <UserNavbar/>
+  const [userEnrolledCourses, setUserEnrolledCourses] = useState([]);
+  //list of courses that the user is enrolled in
+  const { user, isAuthenticated } = useMoralis();
+  const [isLoading, setIsLoading] = useState(true);
+  const router = useRouter();
+   
+
+  useEffect(() => {
+    if (isAuthenticated && user) {
+      loadUserEnrolledCourses();
+    }
+  }, [isLoading, isAuthenticated, user]);
+
+  const loadUserEnrolledCourses = async () => {
+    const EnrolledUserCourses = Moralis.Object.extend("EnrolledUsersCourses");
+    const query = new Moralis.Query(EnrolledUserCourses);
+
+    query.equalTo("user", Moralis.User.current());
+    const result = await query.find();
+
+    if (result[0] != undefined) {
+      console.log(result);
+      setUserEnrolledCourses(result);
+      setIsLoading(false);
+    }
+
+    // we know which courses now we need to make reuqest to moralis to grab all those courses from Course Database
+  };
+
+  return (
+    <div className="  bg-fixed min-h-screen bg-gradient-to-b from-zinc-800    via-emerald-700  to-teal-500 text-white ">
+    <div>
+        {isAuthenticated && user.attributes.role == "instructor" ? (
+          <InstructorNavbar />
+        ) : (
+          <UserNavbar />
+        )}
+      </div>
+
+      {userEnrolledCourses.length != 0 && isAuthenticated && (
+        <div className="flex justify-center ">
+          {/* {JSON.stringify(
+            userEnrolledCourses[0].attributes.course.attributes,
+            0,
+            null
+          )} */}
+          <div className="w-3/4 bg-zinc-800 rounded-3xl  flex-col mt-10">
+            <div className="text-4xl font-extrabold pt-8 pb-4 px-8 justify-center ">
+              My Courses
+            </div>
+            <div className="mx-5 flex flex-wrap  justify-center my-8 ">
+              {userEnrolledCourses.map((course) => (
+                <div className="w-full mx-5 my-3  ">
+                  <div className="flex flex-wrap  justify-between items-center">
+                    <div className="flex-shrink-0   h-20 w-20">
+                      <img
+                        className="h-20 w-20 rounded-full"
+                        src={
+                          course.attributes.course.attributes.image_preview._url
+                        }
+                        alt=""
+                      />
+                    </div>
+                    <div className="text-xl font-semibold my-3 w-2/4 truncate  ">
+                      {course.attributes.course.attributes.name}
+                    </div>
+                    <div className="text-xl font-semibold w-1/4 ">
+                      <span className="text-transparent  bg-clip-text bg-gradient-to-br flex items-center from-teal-500 to-emerald-500 brightness-110 ">
+                        <PlusCircleIcon className="w-5 h-5 mx-1 my-3 sm:my-0 text-emerald-500" />
+                        {course.attributes.rewardsEarned} LRN
+                      </span>
+                    </div>
+                    <div
+                      onClick={() => {
+                        Router.push(
+                          `/user/course/${course.attributes.course.attributes.slug}`
+                        );
+                      }}
+                      className="btn bg-gradient-to-br  from-teal-500 to-emerald-500  hover:scale-95 my-3 sm:my-0  "
+                    >
+                      Go To Course
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+            
+          </div>
         </div>
-    )
+      )}
+    </div>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
