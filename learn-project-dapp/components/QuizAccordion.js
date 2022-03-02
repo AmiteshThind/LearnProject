585d89ceb6ae85b092854f40c4e33e049e9bae13
Amitesh.Thind@coxautoinc.com <Amitesh.Thind@coxautoinc.com>
2022-03-02 18:12:00 -0500

    fixed display bug

--- learn-project-dapp/components/QuizAccordion.js
+++ learn-project-dapp/components/QuizAccordion.js
@@ -6,14 +6,16 @@ function QuizAccordion({ children, section }) {
     const [isOpen, setIsOpen] = useState(false)
 
     return <div>
-        <div onClick={() => setIsOpen(!isOpen)} className="flex transition transform hover:-translate-y-1 justify-between   border-2 border-emerald-500 mx-2 my-3  py-5 font-bold   rounded-2xl">
+        <div onClick={() => setIsOpen(!isOpen)} className="flex transition transform hover:-translate-y-1 justify-between hover:bg-emerald-500   border-2 border-emerald-500 mx-2 my-3  py-5 font-bold cursor-pointer  rounded-2xl">
             <div className='ml-5 text-md'>
             <span>{section +' '+ "Quiz"}</span>
             </div>
-            {!isOpen && <ArrowCircleDownIcon className='h-6 text-emerald-500 w-6 mr-5' />
+            <div>
+            {!isOpen && <ArrowCircleDownIcon className='h-6 text-emerald-500 w-6 mr-5 ' />
             }
             {isOpen && <ArrowCircleUpIcon className='text-emerald-500  h-6 w-6 mr-5 ' />
             }
+            </div>
         </div>
         <div className={`   mt-2 rounded-3xl  ${isOpen ? 'd-block' : 'hidden'}`}>
             {children}
