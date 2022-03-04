a1c854a11b43367b2f6d42af11955f578758819a
Amitesh.Thind@coxautoinc.com <thindamitesh@gmail.com>
2022-03-04 01:12:38 -0500

    updated styling

--- learn-project-dapp/components/UserQuiz.js
+++ learn-project-dapp/components/UserQuiz.js
@@ -2,20 +2,30 @@ import React, { useEffect, useState } from "react";
 import { Moralis } from "moralis";
 import toast, { Toaster } from "react-hot-toast";
 
-function UserQuiz({ availableQuizSections, claimLearn, setRewardsEarned,rewardsEarned, setRewardsClaimed,section, quizQuestions, user, course }) {
+function UserQuiz({
+  availableQuizSections,
+  claimLearn,
+  setRewardsEarned,
+  rewardsEarned,
+  setRewardsClaimed,
+  section,
+  quizQuestions,
+  user,
+  course,
+}) {
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [showScore, setShowScore] = useState(false);
   const [score, setScore] = useState({});
   const [quizStarted, setQuizStarted] = useState(false);
   const [completedQuizzes, setCompletedQuizzes] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  const [rewardRate,setRewardRate] = useState(1)
-
+  const [rewardRate, setRewardRate] = useState(1);
+  const [optionSelected, setOptionSelected] = useState("");
 
   useEffect(() => {
     checkIsQuizCompleted();
-    if(course[0].attributes.paid){
-        setRewardRate(2);
+    if (course[0].attributes.paid) {
+      setRewardRate(2);
     }
   }, [isLoading]);
 
@@ -33,35 +43,34 @@ function UserQuiz({ availableQuizSections, claimLearn, setRewardsEarned,rewardsE
     }
   };
 
- 
-
   const handleAnswerOptionClick = (optionSelected) => {
     //check if question is correct nad update score
-    console.log("here")
+    console.log("here");
+    setOptionSelected(optionSelected);
     const newScore = Object.assign({}, score); // pass this object to compelte quiz to get updated state()
     if (optionSelected == quizQuestions[currentQuestion].attributes.answer) {
-     console.log("CORRECTANSWER")
-      
-      newScore[section] = score[section]+1;
+      console.log("CORRECTANSWER");
+
+      newScore[section] = score[section] + 1;
       setScore(newScore);
-      console.log("NEWSCORE: "+newScore[section])
-
-
-      
+      console.log("NEWSCORE: " + newScore[section]);
     }
+
+    setTimeout(() => {
+      const nextQuestion = currentQuestion + 1;
+      if (nextQuestion < quizQuestions.length) {
+        setCurrentQuestion(nextQuestion);
+      } else {
+        setShowScore(true);
+        completedQuiz(newScore);
+      }
+
+      setOptionSelected("");
+    }, 3000);
     // incrment current question to next question
-
-    const nextQuestion = currentQuestion + 1;
-    if (nextQuestion < quizQuestions.length) {
-      setCurrentQuestion(nextQuestion);
-    } else {
-      setShowScore(true);
-      completedQuiz(newScore);
-    }
   };
 
   const completedQuiz = async (newScore) => {
-
     const EnrolledUserCourses = Moralis.Object.extend("EnrolledUsersCourses");
     const query = new Moralis.Query(EnrolledUserCourses);
     query.equalTo("user", user);
@@ -71,33 +80,37 @@ function UserQuiz({ availableQuizSections, claimLearn, setRewardsEarned,rewardsE
     let arr = completedQuizzes;
     arr.push(section);
     objectToUpdate.set("completedQuizzes", arr);
-    objectToUpdate.set("quizScore",newScore);
-    objectToUpdate.set("rewardsEarned",rewardsEarned+newScore[section]*rewardRate)
+    objectToUpdate.set("quizScore", newScore);
+    objectToUpdate.set(
+      "rewardsEarned",
+      rewardsEarned + newScore[section] * rewardRate
+    );
     //await objectToUpdate.save();
     setCompletedQuizzes((oldArray) => [...oldArray, section]);
-    setRewardsEarned(rewardsEarned+newScore[section]*rewardRate)
+    setRewardsEarned(rewardsEarned + newScore[section] * rewardRate);
 
     //check course completed and give rewards based on that
-    if(completedQuizzes.length == availableQuizSections.length){
-        //course completed
-      objectToUpdate.set("rewardsEarned",rewardsEarned+25*rewardRate+newScore[section]*rewardRate) //25+ learn token or 50 plus learn token if paid course on completion of course (value subject to change based on tokenomics)
-      toast.success("Course Completed. Earned Extra " +25*rewardRate + "$LEARN") 
-      setRewardsEarned(rewardsEarned+newScore[section]*rewardRate+25*rewardRate)
+    if (completedQuizzes.length == availableQuizSections.length) {
+      //course completed
+      objectToUpdate.set(
+        "rewardsEarned",
+        rewardsEarned + 25 * rewardRate + newScore[section] * rewardRate
+      ); //25+ learn token or 50 plus learn token if paid course on completion of course (value subject to change based on tokenomics)
+      toast.success(
+        "Course Completed. Earned Extra " + 25 * rewardRate + "$LEARN"
+      );
+      setRewardsEarned(
+        rewardsEarned + newScore[section] * rewardRate + 25 * rewardRate
+      );
     }
-    await objectToUpdate.save()
-
+    await objectToUpdate.save();
   };
-
- 
-
 
   return (
     <div className=" w-4/5   rounded-3xl  text-white ">
       <div class="card lg:card-side bg-zinc-800 sm:h-[44rem] md:h-[38rem lg:h-[40rem] shadow-xl flex flex-col ">
         {quizStarted && !showScore && !completedQuizzes.includes(section) ? (
-           
           <div className="w-full ">
-                 
             <div className=" flex items-center  flex-col flex-wrap w-3/8   p-5">
               <div className="border  w-full  mb-5 rounded-3xl p-3 border-emerald-500 text-white">
                 <span className="text-emerald-500 font-extrabold">NOTE: </span>
@@ -118,8 +131,17 @@ function UserQuiz({ availableQuizSections, claimLearn, setRewardsEarned,rewardsE
               {quizQuestions[currentQuestion].attributes.options.map(
                 (option) => (
                   <div
-                    onClick={() => handleAnswerOptionClick(option)}
-                    className="btn my-2 border hover:bg-emerald-500 border-zinc-600 bg-zinc-800 "
+                    onClick={() => {
+                      handleAnswerOptionClick(option);
+                    }}
+                    className={`btn my-2 border hover:scale-100 border-zinc-600 bg-zinc-800 ${
+                      optionSelected &&
+                      option == quizQuestions[currentQuestion].attributes.answer
+                        ? `bg-emerald-500 hover:bg-emerald-500`
+                        : optionSelected == option
+                        ? `bg-red-400 hover:bg-red-400`
+                        : ""
+                    }`}
                   >
                     {option}
                   </div>
@@ -127,12 +149,10 @@ function UserQuiz({ availableQuizSections, claimLearn, setRewardsEarned,rewardsE
               )}
             </div>
           </div>
-        ) :  !completedQuizzes.includes(section) &&
-        !quizStarted &&
-          !showScore 
-          ? (
+        ) : !completedQuizzes.includes(section) &&
+          !quizStarted &&
+          !showScore ? (
           <div className="w-full ">
-                
             <div className=" flex items-center  flex-col flex-wrap w-3/8   p-5">
               <div className="mt-10 text-5xl text-emerald-500 font-extrabold">
                 {section} Quiz
@@ -182,30 +202,35 @@ function UserQuiz({ availableQuizSections, claimLearn, setRewardsEarned,rewardsE
               <div class="shadow stats  border-emerald-500 border stats-vertical">
                 <div class="stat bg-zinc-800 text-white">
                   <div class="stat-title">Rewards Earned</div>
-                  <div class="stat-value">{score[section]*rewardRate}</div>
+                  <div class="stat-value">{score[section] * rewardRate}</div>
                   <div class=" stat-desc">$LEARN</div>
                 </div>
 
                 <div class="stat bg-zinc-800 text-white">
                   <div class="stat-title">Score</div>
-                  <div class="stat-value">{score[section]}/{quizQuestions.length}</div>
+                  <div class="stat-value">
+                    {score[section]}/{quizQuestions.length}
+                  </div>
                 </div>
 
                 <div class="stat bg-zinc-800 text-white">
                   <div class="stat-title">Percentage</div>
-                  <div class="stat-value">{Math.round(
-                        (score[section] / quizQuestions.length) * 100
-                      )}%</div>
+                  <div class="stat-value">
+                    {Math.round((score[section] / quizQuestions.length) * 100)}%
+                  </div>
                 </div>
               </div>
             </div>
-            <div onClick={()=>claimLearn()} className="p-6 text-xl bg-emerald-500 my-10 rounded-xl font-extrabold hover:scale-95 cursor-pointer duration-150 ">
+            <div
+              onClick={() => claimLearn()}
+              className="p-6 text-xl bg-emerald-500 my-10 rounded-xl font-extrabold hover:scale-95 cursor-pointer duration-150 "
+            >
               CLAIM $LEARN
             </div>
           </div>
         )}
       </div>
-      <Toaster/>
+      <Toaster />
     </div>
   );
 }
