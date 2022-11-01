12a3219e6f212d3f136e4d0f8051e3341bf70037
Amitesh Thind <31372481+AmiteshThind@users.noreply.github.com>
2022-10-31 22:19:48 -0400

    Update index.js

--- server/middleware/index.js
+++ server/middleware/index.js
@@ -2,6 +2,6 @@ import expressJwt from 'express-jwt'
 
 export const requireSignIn = expressJwt({
     getToken: (req,res) => req.cookies.token,
-    secret: "DYAHUIYFFGJI8934KOPEIRMFNTI6N34NB98RTJIHGT678IKNBY789OLJR34R",
+    secret: "update with env key",
     algorithms: ["HS256"] 
 })
